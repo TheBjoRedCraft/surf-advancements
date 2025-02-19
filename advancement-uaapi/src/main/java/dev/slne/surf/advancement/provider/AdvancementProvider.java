ea563b45cadb4cb57997095085226f58e11bfebd
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-02-19 20:54:41 +0100

    feat: add generalrootadvancement with gradient

--- advancement-uaapi/src/main/java/dev/slne/surf/advancement/provider/AdvancementProvider.java
+++ advancement-uaapi/src/main/java/dev/slne/surf/advancement/provider/AdvancementProvider.java
@@ -5,28 +5,35 @@ import com.fren_gor.ultimateAdvancementAPI.advancement.BaseAdvancement;
 import com.fren_gor.ultimateAdvancementAPI.advancement.RootAdvancement;
 import dev.slne.surf.advancement.SurfAdvancement;
 import dev.slne.surf.advancement.impl.tabs.general.GeneralRootAdvancement;
-import dev.slne.surf.advancement.impl.tabs.general.GeneralTestAdvancement;
 import lombok.Getter;
 
 @Getter
 public class AdvancementProvider {
   private AdvancementTab generalTab;
+  private AdvancementTab eventTab;
+  private AdvancementTab survivalTab;
+
   private final String generalNamespace = "surf_general";
+  private final String eventNamespace = "surf_event";
+  private final String survivalNamespace = "surf_survival";
 
   private RootAdvancement generalRootAdvancement;
-  private BaseAdvancement generalTestAdvancement;
+
+  private RootAdvancement eventRootAdvancement;
+
+  private RootAdvancement survivalRootAdvancement;
 
 
   public void registerGeneral() {
     generalTab = SurfAdvancement.getInstance().getAdvancementAPI().createAdvancementTab(generalNamespace);
+    eventTab = SurfAdvancement.getInstance().getAdvancementAPI().createAdvancementTab(eventNamespace);
+    survivalTab = SurfAdvancement.getInstance().getAdvancementAPI().createAdvancementTab(survivalNamespace);
 
     this.generalRootAdvancement = new RootAdvancement(this.getGeneralTab(), this.generalNamespace + ".root", new GeneralRootAdvancement(), "textures/block/stone.png");
-    this.generalTestAdvancement = new BaseAdvancement(this.generalNamespace + ".test", new GeneralTestAdvancement(), this.generalRootAdvancement);
 
 
     generalTab.registerAdvancements(
-        this.generalRootAdvancement,
-        this.generalTestAdvancement
+        this.generalRootAdvancement
     );
     generalTab.automaticallyShowToPlayers();
   }
